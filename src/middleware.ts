06baa9c54631a6f578e76ac1107807f06a3fc4f7
Eric Nichols <ebn646@hotmail.com>
2024-10-25 12:56:28 -0400

    feat: new clerk login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/api(.*)', '/forum(.*)'])
 
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    auth().protect();
-  }
-  return NextResponse.next();
-});
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
+
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/"],
